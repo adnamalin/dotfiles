80a74fcc16c150c2a37153057d27fca75ad8aaa4
Amanda Lin <adnama.lin@gmail.com>
2021-06-21 13:29:56 -0500

    copy pasta

--- zsh/install-tool.sh
+++ zsh/install-tool.sh
@@ -44,7 +44,7 @@ else
 fi
 
 # # Set zsh as default shell
-sudo chsh -s $(which zsh)
+# sudo chsh -s $(which zsh)
 
 # Install Oh My Zsh
 
@@ -57,8 +57,14 @@ fi
 # https://github.com/ohmyzsh/ohmyzsh#unattended-install
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
 
+# Install Pure Prompt
+# https://github.com/sindresorhus/pure#manually
+mkdir -p "$HOME/.zsh"
+git clone https://github.com/sindresorhus/pure.git "$HOME/.zsh/pure"
+
 # Install zsh-autosuggestions
 # https://github.com/zsh-users/zsh-autosuggestions/blob/master/INSTALL.md#oh-my-zsh
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+
 
 cp zsh/.zshrc ~
